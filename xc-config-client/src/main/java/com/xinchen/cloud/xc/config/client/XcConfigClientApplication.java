25f57a506581a825c5119818b75e48ce4571d9be
melodyfff <307208327@qq.com>
2018-09-18 00:02:49 +0800

    :coffee: init config client

---
+++ xc-config-client/src/main/java/com/xinchen/cloud/xc/config/client/XcConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.xinchen.cloud.xc.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XcConfigClientApplication.class, args);
+    }
+}
