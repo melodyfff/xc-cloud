2ff2c6e0aa26b94dbe0e77d39299e9053535cdfc
melodyfff <307208327@qq.com>
2018-11-09 15:19:05 +0800

    :smile: change name

--- xc-config-client/src/main/java/com/xinchen/cloud/xc/config/client/ConfigClient.java
+++ xc-config-client/src/main/java/com/xinchen/cloud/xc/config/client/ConfigClient.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XcConfigClientApplication {
+public class ConfigClient {
 
     public static void main(String[] args) {
-        SpringApplication.run(XcConfigClientApplication.class, args);
+        SpringApplication.run(ConfigClient.class, args);
     }
 }
--- xc-config/src/main/java/com/xinchen/cloud/xcconfig/ConfigService.java
+++ xc-config/src/main/java/com/xinchen/cloud/xcconfig/ConfigService.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class XcConfigApplication {
+public class ConfigService {
 
     public static void main(String[] args) {
-        SpringApplication.run(XcConfigApplication.class, args);
+        SpringApplication.run(ConfigService.class, args);
     }
 }
