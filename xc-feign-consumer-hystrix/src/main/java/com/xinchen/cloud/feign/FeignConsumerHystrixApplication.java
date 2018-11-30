3fbb6747a2540ff75f512eee8e2bf5c9606546c8
melodyfff <307208327@qq.com>
2018-12-01 00:40:21 +0800

    :smile: Spring Cloud（四）断路器监控(Hystrix Dashboard) Hystrix Dashboard

--- xc-feign-consumer-hystrix/src/main/java/com/xinchen/cloud/feign/FeignConsumerHystrixApplication.java
+++ xc-feign-consumer-hystrix/src/main/java/com/xinchen/cloud/feign/FeignConsumerHystrixApplication.java
@@ -1,9 +1,13 @@
 package com.xinchen.cloud.feign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 消费者 Feign
@@ -12,9 +16,30 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 29/11/2018 19:50
  */
 @EnableFeignClients
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
 public class FeignConsumerHystrixApplication {
+
+    /**
+     * 出现 Unable to connect to Command Metric Stream. 时参考以下博客
+     * https://www.cnblogs.com/mark7/p/8920288.html
+     *
+     * 访问 http://localhost:9000/hystrix/ 查看详情
+     *  http://127.0.0.1:9000/hystrix.stream
+     *  2000   hello
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(FeignConsumerHystrixApplication.class, args);
--- xc-ribbon-consumer-hystrix/src/main/java/com/xinchen/cloud/ribbon/RibbonConsumerHystrixApplication.java
+++ xc-ribbon-consumer-hystrix/src/main/java/com/xinchen/cloud/ribbon/RibbonConsumerHystrixApplication.java
@@ -1,10 +1,13 @@
 package com.xinchen.cloud.ribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 29/11/2018 17:04
  */
 @EnableHystrix
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
 public class RibbonConsumerHystrixApplication {
@@ -26,7 +30,24 @@ public class RibbonConsumerHystrixApplication {
         return new RestTemplate();
     }
 
-
+    /**
+     * 出现 Unable to connect to Command Metric Stream. 时参考以下博客
+     * https://www.cnblogs.com/mark7/p/8920288.html
+     *
+     * 访问 http://localhost:9001/hystrix/ 查看详情
+     *  http://127.0.0.1:9001/hystrix.stream
+     *  2000   hello
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RibbonConsumerHystrixApplication.class, args);
