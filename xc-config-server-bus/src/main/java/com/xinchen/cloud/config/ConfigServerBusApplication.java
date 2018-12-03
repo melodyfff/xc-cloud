e2a7cbf24a7df94baf603e5b3934140ed9a249b5
melodyfff <307208327@qq.com>
2018-12-03 17:19:44 +0800

    :smile: Spring Cloud（九）高可用的分布式配置中心 Spring Cloud Bus 消息总线集成（RabbitMQ）

---
+++ xc-config-server-bus/src/main/java/com/xinchen/cloud/config/ConfigServerBusApplication.java
@@ -0,0 +1,22 @@
+package com.xinchen.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Xin Chen (xinchenmelody@gmail.com)
+ * @version 1.0
+ * @date Created In 2018/12/2 22:43
+ */
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerBusApplication.class, args);
+    }
+
+}
